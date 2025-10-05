44017299cdfe4889ad0966c0c2972b2e02e6bbc7
Shubhrajit1pallob <spallob@asu.edu>
2025-10-05 00:18:14 -0700

    Set up the database with Neon and Drizzle ORM.

---
+++ drizzle/0000_kind_bedlam.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar(256) NOT NULL,
+	"email" varchar(256) NOT NULL,
+	"password" varchar(256) NOT NULL,
+	"role" varchar(256) NOT NULL,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
